c65c16ab90c11eb9e1a406bd30868f01e4a9f5e3
MiNHAZ-33 <miminhaz33@gmail.com>
2024-03-01 10:36:54 +0600

    code refractored

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,19 +3,16 @@ class MessagesController < ApplicationController
     if params[:message][:body].empty?
       return
     end
-    room_name = get_name(params[:message][:sender_id], params[:message][:receiver_id])
-    room = Room.where(name: room_name).first
-    Message.create(body: params[:message][:body], sender_id: params[:message][:sender_id], receiver_id: params[:message][:receiver_id], room: room)
+    room = Room.where(name: msg_params[:room_id]).first
+    message = Message.new(msg_params)
+    message.room = room
+    message.save!
+
   end
 
   private
   def msg_params
-    params.require(:message).permit(:body, :receiver_id, :sender_id)
-  end
-
-  def get_name(user_1, user_2)
-    user = [user_1, user_2].sort
-    "private_#{user[0]}_#{user[1]}"
+    params.require(:message).permit(:body, :receiver_id, :sender_id, :room_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  include UsersHelper
   before_action :set_user
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_user_id
   def show
     @users = User.all_except(current_user)
-    @room_name = get_name(@user, current_user)
-    @single_room = Room.where(name: @room_name).first || Room.create_private_room(@room_name)
-    @messages = @single_room.messages.order(created_at: :asc)
+    @room_name = get_name_by_id(current_user.id, @user.id)
+    @room = Room.where(name: @room_name).first || Room.create_private_room(@room_name)
+    @messages = @room.messages.order(created_at: :asc)
     @message = Message.new
     render 'homes/index'
   end
@@ -13,12 +14,6 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find(params[:id])
-  end
-
-  private
-  def get_name(user_1, user_2)
-    user = [user_1, user_2].sort
-    "private_#{user[0].id}_#{user[1].id}"
   end
 
   def invalid_user_id
