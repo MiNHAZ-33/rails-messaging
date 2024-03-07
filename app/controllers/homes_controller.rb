350b2c776b8450fe2229c91cd2fd17d611cdb593
MiNHAZ-33 <miminhaz33@gmail.com>
2024-03-07 10:20:58 +0600

    cleanup home index action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,9 +2,6 @@ class HomesController < ApplicationController
   # GET /homes or /homes.json
   def index
     @users = User.all_except(current_user)
-    @user = User.find_by_id(params[:id])
-    @message = Message.new
-    @current_user = @user
   end
 
 end
